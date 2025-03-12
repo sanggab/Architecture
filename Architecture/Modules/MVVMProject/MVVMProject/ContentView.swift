292f20b338c66fcf3e2a054e17f451be4cc758c0
심상갑 <kappa2410@gmail.com>
2025-03-12 23:31:02 +0900

    Feat: SwiftUI + MVVM에 대한 고찰

--- Architecture/Modules/MVVMProject/MVVMProject/ContentView.swift
+++ Architecture/Modules/MVVMProject/MVVMProject/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 import Combine
 
+/// SwiftUI에서 MVVM 구조가 필요한가에 대한 나의 생각
+/// MVVM의 ViewModel은 데이터를 바인딩해주고 비즈니스 로직을 처리하고 View에 보여줄 데이터를 가공하는 역할.
+/// 하지만 SwiftUI에서는 Property Wrapper라는 데이터 바인딩을 해주는 어노테이션이 존재하는 데, VM의 왜 필요할까라고 생각이 들게 됨
+/// 그래서 SwiftUI에서 VM을 구현해보기로함.
+/// 구현한 결과, SwiftUI 강점인 Property Wrapper, Modifier를 사용을 못 함.
+/// State의 업데이트를 위해서 Property Wrapper을 제공해줬는데 단순하게 oneText를 업데이트를 하면 되는 것을 바인딩을 거쳐서 업데이트해줌.
+/// 그래서 내가 SwiftUI에서 MVVM은 안 맞는다고 생각하는 이유.
+///
+/// 내가 생각한 결론:
+/// 결국 SwiftUI에서는 Data Binding이라는 애는 @State같은 Property Wrapper들이 해주고, View는 Body의 영역이 해주고 비즈니스 로직은 func를 만들어서 처리할거니까 이 제차게 MVVM 구조가 아닌가라고 생각이 든다.
 struct ContentView: View {
+    
+    /// VM
     var viewModel: ViewModel
     
+    /// SwiftUI 같은 선언형 UI는 명령형 UI와 달리 UI의 상태 변화를 직접 알려줄 필요가 없음
+    /// 하지만 MVVM 구조로 가져간다면 Output을 바인딩해서 UI에 알려줘야 할 의무가 있음
+    /// 그래서 Output하고 바인딩할 객체를 만듬.
+    /// 하지만 Struct이기 때문에 어노테이션이 없는 객체들을 만들기 힘듬.
+    /// 그래서 @State 객체를 만듬.
     @State private var oneText: String = ""
     @State private var twoText: String = ""
     @State private var threeText: String = ""
@@ -60,11 +77,13 @@ struct ContentView: View {
             }
         }
         .task {
+            /// VM과 V에서 객체들을 바인딩 시켜줌.
             bind()
             viewModel.bind()
         }
     }
     
+    /// VM의 Output을 바인딩을 해주는 메소드
     func bind() {
         viewModel.output._oneOutput
             .sink { output in
--- Architecture/Modules/MVVMProject/MVVMProject/ViewModel/ViewModel.swift
+++ Architecture/Modules/MVVMProject/MVVMProject/ViewModel/ViewModel.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 import Combine
 
+/// VM의 Input을 인터페이스화 시키면 Action하고 다를 바 없음
+/// 그러면 ``ViewModel/Output-swift.struct``은 결국엔 State가 되버림
+/// 즉 MVI 구조가 되버린다.
+protocol ViewModelInputInterface {
+    func one()
+    func two()
+    func three()
+    func four()
+}
+
 enum TestCase {
     case create
     case read
     case update
     case delete
 }
-
+/// VM
+///
 final class ViewModel {
-    
+    /// View의 Input 모델
+    /// Input 모델을 인터페이스로 만들면 안되는 이유 ``ViewModelInputInterface``
     struct Input {
         var one: PassthroughSubject<String, Never> = .init()
         var two: PassthroughSubject<Bool, Never> = .init()
@@ -24,6 +36,14 @@ final class ViewModel {
         var four: PassthroughSubject<TestCase, Never> = .init()
     }
     
+//    enum Input2 {
+//        case one
+//        case two
+//        case three
+//        case four
+//    }
+    
+    /// View의 Output 모델
     struct Output {
         var _oneOutput: PassthroughSubject<String, Never> = .init()
         var _twoOutput: PassthroughSubject<Bool, Never> = .init()
@@ -38,7 +58,7 @@ final class ViewModel {
     init() {
         
     }
-    
+    /// Input모델과 Output모델을 연결
     func bind() {
         let test: PassthroughSubject<TestCase, Never> = input.four
         
@@ -78,5 +98,23 @@ final class ViewModel {
                 self.output._threeOutput.send(value)
             }
             .store(in: &cancellable)
+    }
+}
+
+extension ViewModel: ViewModelInputInterface {
+    func one() {
+        self.output._oneOutput.send("원이다")
+    }
+    
+    func two() {
+        self.output._oneOutput.send("둘이다")
+    }
+    
+    func three() {
+        self.output._oneOutput.send("셋이다")
+    }
+    
+    func four() {
+        self.output._oneOutput.send("원이다")
     }
 }
